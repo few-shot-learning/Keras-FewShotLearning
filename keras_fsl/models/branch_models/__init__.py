a58f61f295e0fa4950b91514e4af8968a6d2e2e7
Clément Walter <clementw@sicara.com>
2019-10-04 15:15:05 +0200

    Add Darknet53 model

--- keras_fsl/models/branch_models/__init__.py
+++ keras_fsl/models/branch_models/__init__.py
@@ -1,4 +1,5 @@
-from tensorflow.python.keras.applications import *
-from .single_conv_2d import SingleConv2D
+from .darknet_53 import Darknet53
 from .koch_net import KochNet
+from .single_conv_2d import SingleConv2D
+from tensorflow.python.keras.applications import *
 from .vinyals_net import VinyalsNet
