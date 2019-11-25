5a65d7d9a30455ea205f67eeeab084fdbd787b1d
Clément Walter <clementw@sicara.com>
2019-11-25 19:16:45 +0100

    Fix layer with tf.function decorator and reduce_max(encoding)

--- keras_fsl/losses/classification_loss.py
+++ keras_fsl/losses/classification_loss.py
@@ -5,19 +5,25 @@ from tensorflow.python.keras.layers import Layer
 
 class ClassificationLoss(Layer):
 
-    def __init__(self, loss, metric_layer, **kwargs):
+    def __init__(self, loss, similarity_layer, **kwargs):
+        """
+        Args:
+            loss: categorical loss function to be used on the predictions
+            similarity_layer: function to compute pair-wise similarity. Score is assumed to be normalized between 0 and 1 and
+                1 meaning same and 0 different. This similarity can be learnt during training when passed a layer.
+        """
         super().__init__(**kwargs)
         self.loss = loss
-        self.metric_layer = metric_layer
-        if len(self.metric_layer.inputs) != 2:
-            raise NotImplementedError('This loss currently supports only metric_later with two inputs')
+        self.similarity_layer = similarity_layer
+        if len(self.similarity_layer.inputs) != 2:
+            raise NotImplementedError('This loss currently supports only similarity_layer with two inputs')
         self.indexes = None
 
     def get_config(self):
         config = super().get_config()
         config.update({
             'loss': self.loss,
-            'metric_layer': self.metric_layer,
+            'similarity_layer': self.similarity_layer,
         })
         return config
 
@@ -29,6 +35,7 @@ class ClassificationLoss(Layer):
     def compute_output_shape(self, input_shape):
         return len(self.indexes),
 
+    @tf.function
     def call(self, inputs, **kwargs):
         """
         Args:
@@ -37,14 +44,20 @@ class ClassificationLoss(Layer):
         """
         embeddings, labels = inputs
         indexes = tf.meshgrid(*[
-            self.indexes for _ in range(len(self.metric_layer.inputs))
+            self.indexes for _ in range(len(self.similarity_layer.inputs))
         ])
-        scores_matrix = tf.reshape(self.metric_layer([
+        scores_matrix = tf.reshape(self.similarity_layer([
             tf.gather(embeddings, tf.reshape(indexes[i], [-1])) for i in range(len(indexes))
         ]), [len(self.indexes), len(self.indexes)])
-        tf.linalg.set_diag(scores_matrix, [0] * len(self.indexes))  # do not select same image score
-        best_match = tf.argmax(scores_matrix, axis=1)
-        predicted_labels = tf.gather(labels, best_match)
-        loss = self.loss(labels, predicted_labels)
+        scores_matrix = tf.linalg.set_diag(scores_matrix, [0] * len(self.indexes))  # do not select same image score
+        classes, encoding = tf.unique(tf.reshape(labels, [-1]))
+        labels_one_hot = tf.one_hot(encoding, depth=tf.reduce_max(encoding))
+        loss = self.loss(
+            labels_one_hot,
+            tf.nn.softmax(
+                tf.linalg.matmul(scores_matrix, labels_one_hot) / tf.reduce_sum(labels_one_hot, axis=0),
+                axis=1,
+            ),
+        )
         self.add_loss(tf.reduce_mean(loss), inputs=True)
         return loss
