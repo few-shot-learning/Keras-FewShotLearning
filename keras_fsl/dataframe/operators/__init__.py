c34bb247d0a20decd2fb5c20db3eaa2b324aaefe
Clément Walter <clementw@sicara.com>
2020-03-09 16:50:57 +0100

    Remove wrongs imports in dataframe ops

--- keras_fsl/dataframe/operators/__init__.py
+++ keras_fsl/dataframe/operators/__init__.py
@@ -1,15 +1,9 @@
-from .center_coordinates_to_keypoint import CenterCoordinatesToKeypoint
-from .coordinates_to_bounding_box import CoordinatesToBoundingBox
-from .corner_to_center_coordinates import CornerToCenterCoordinates
 from .naive_max_proba import NaiveMaxProba
 from .random_assignment import RandomAssignment
 from .to_k_shot_dataset import ToKShotDataset
 
 
 __all__ = [
-    'CenterCoordinatesToKeypoint',
-    'CoordinatesToBoundingBox',
-    'CornerToCenterCoordinates',
     'NaiveMaxProba',
     'RandomAssignment',
     'ToKShotDataset',
