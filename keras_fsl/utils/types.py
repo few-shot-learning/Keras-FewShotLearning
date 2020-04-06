89d992c2ae3825023e788c77e7af3ea5fa6e7228
Arnault Chazareix <arnaultc@sicara.com>
2020-04-06 10:20:42 +0200

    add tf.Tensor shape types

--- keras_fsl/utils/types.py
+++ keras_fsl/utils/types.py
@@ -1,5 +1,8 @@
-from typing import Mapping, Type
+from typing import Mapping, Type, List
 import tensorflow as tf
 
 TF_TENSOR = Type[tf.Tensor]
+TENSOR_DTYPE_STR = str
+TENSOR_SHAPE = List[int]
+TENSOR_NDIM = int
 TENSOR_MAP = Mapping[str, TF_TENSOR]
