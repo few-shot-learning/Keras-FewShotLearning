de8ae5382f5eedf39371a8691f7df8919d6f74fe
Arnault Chazareix <arnaultc@sicara.com>
2020-04-07 11:55:44 +0200

    black

--- keras_fsl/callbacks/tensorboard_save_figures.py
+++ keras_fsl/callbacks/tensorboard_save_figures.py
@@ -4,6 +4,7 @@ from typing import Mapping, Callable
 from matplotlib import pyplot as plt
 import tensorflow as tf
 
+
 class TensorBoardSaveFigures(tf.keras.callbacks.Callback):
     def __init__(self, logdir, figure_producers: Mapping[str, Callable[[tf.keras.Model, int], plt.Figure]]):
         super().__init__()
@@ -21,9 +22,8 @@ class TensorBoardSaveFigures(tf.keras.callbacks.Callback):
         """Converts the matplotlib plot specified by 'figure' to a uint8 tensor image and
         returns it. The supplied figure is closed and inaccessible after this call."""
         buffer = io.BytesIO()
-        plt.savefig(buffer, format='png')
+        plt.savefig(buffer, format="png")
         plt.close(figure)
         buffer.seek(0)
         image = tf.image.decode_png(buffer.getvalue(), channels=4)
         return tf.expand_dims(image, 0)
-
