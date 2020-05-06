a6bc99e8c59e1078a8acbf3f379c447796ad84a5
Clément Walter <clementw@sicara.com>
2020-05-06 10:32:41 +0200

    Add StackedHead and simplify MixedNorms

--- keras_fsl/models/head_models/__init__.py
+++ keras_fsl/models/head_models/__init__.py
@@ -2,6 +2,7 @@ from .dense_sigmoid import DenseSigmoid
 from .mixed_norms import MixedNorms
 from .norm_head import Norm
 from .proto_nets import ProtoNets
+from .stacked_head import StackedHead
 from .triplet_loss import TripletLoss
 
-__all__ = ["DenseSigmoid", "MixedNorms", "Norm", "ProtoNets", "TripletLoss"]
+__all__ = ["DenseSigmoid", "MixedNorms", "Norm", "ProtoNets", "TripletLoss", "StackedHead"]
--- keras_fsl/models/head_models/stacked_head.py
+++ keras_fsl/models/head_models/stacked_head.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 from tensorflow.keras import activations
 from tensorflow.keras.layers import (
     Concatenate,
@@ -7,42 +6,29 @@ from tensorflow.keras.layers import (
     Flatten,
     GlobalAveragePooling2D,
     Input,
-    Lambda,
     Reshape,
 )
 from tensorflow.keras.models import Model
 
 
-def MixedNorms(input_shape, norms=None, use_bias=True, activation="sigmoid"):
+def StackedHead(input_shape, use_bias=True, activation="sigmoid"):
     """
-    Head inspired by
-    [this kaggle notebook](https://www.kaggle.com/martinpiotte/whale-recognition-model-with-score-0-78563)
-    on whale identification
-
+    Learn the linear coordinate wise linear combination
     Args:
         input_shape (tuple): arg to be passed to keras.layer.Input
-        norms (List[function]): list of function to be applied to the list of tensors [query, support] in a Lambda layer
         use_bias (bool), whether to use bias in layers or not
         activation: add an activation function as in other keras standard layers. Default to sigmoid to output a normalize score
     """
-    if norms is None:
-        norms = [
-            lambda x: x[0] * x[1],
-            lambda x: x[0] + x[1],
-            lambda x: tf.math.abs(x[0] - x[1]),
-            lambda x: tf.math.square(x[0] - x[1]),
-        ]
-
     query = Input(input_shape)
     support = Input(input_shape)
     inputs = [query, support]
     if len(input_shape) == 4:
         inputs = [GlobalAveragePooling2D()(input_) for input_ in inputs]
 
-    output = Concatenate()([Lambda(norm)(inputs) for norm in norms])
-    output = Reshape((len(norms), input_shape[-1], 1), name="stack")(output)
+    output = Concatenate()(inputs)
+    output = Reshape((len(inputs), input_shape[-1], 1), name="stack")(output)
 
-    output = Conv2D(filters=32, kernel_size=(len(norms), 1), activation="relu", name="norms_selection", use_bias=use_bias)(
+    output = Conv2D(filters=32, kernel_size=(len(inputs), 1), activation="relu", name="norms_creation", use_bias=use_bias)(
         output
     )
     output = Conv2D(filters=1, kernel_size=(1, 1), activation="linear", name="norms_average", use_bias=use_bias)(output)
