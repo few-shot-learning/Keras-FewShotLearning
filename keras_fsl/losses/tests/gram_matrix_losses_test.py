f1bb9cf9ce3ab0a4c20ae846e48c1db19330af06
Clément Walter <clementw@sicara.com>
2020-05-11 19:30:04 +0200

    Add ClassConsistencyLoss test

--- keras_fsl/losses/tests/gram_matrix_losses_test.py
+++ keras_fsl/losses/tests/gram_matrix_losses_test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pytest
 import tensorflow as tf
 
-from keras_fsl.losses.gram_matrix_losses import MeanScoreClassificationLoss, TripletLoss
+from keras_fsl.losses.gram_matrix_losses import ClassConsistencyLoss, MeanScoreClassificationLoss, TripletLoss
 from keras_fsl.utils.tensors import get_dummies
 
 
@@ -72,6 +72,34 @@ class TestGramMatrixLoss:
                 / batch_size
             )
             tf_loss = MeanScoreClassificationLoss(unsupervised)(
+                tf.convert_to_tensor(y_true, tf.float32), tf.convert_to_tensor(y_pred, tf.float32)
+            )
+            np.testing.assert_almost_equal(tf_loss, np_loss, decimal=5)
+
+    class TestClassConsistencyLoss:
+        @pytest.mark.parametrize("unsupervised", (True, False))
+        def test_loss_should_equal_literal_calculation(self, unsupervised):
+            batch_size = 16
+            labels = np.random.choice(["a", "b", "c"], batch_size)
+            y_true = pd.get_dummies(labels).values
+            y_pred = np.random.rand(batch_size, batch_size)
+            classes_scores = (
+                pd.melt(
+                    pd.DataFrame(
+                        y_pred, columns=pd.Index(labels, name="support_label"), index=pd.Index(labels, name="query_label"),
+                    ).reset_index(),
+                    id_vars=["query_label"],
+                )
+                .groupby(["query_label", "support_label"])
+                .agg("mean")
+                .unstack("support_label")
+                .values
+            )
+            np_loss = np.eye(3) * np.log(classes_scores)
+            if not unsupervised:
+                np_loss += (1 - np.eye(3)) * np.log(1 - classes_scores)
+            np_loss = -np.mean(np_loss)
+            tf_loss = ClassConsistencyLoss(unsupervised)(
                 tf.convert_to_tensor(y_true, tf.float32), tf.convert_to_tensor(y_pred, tf.float32)
             )
             np.testing.assert_almost_equal(tf_loss, np_loss, decimal=5)
