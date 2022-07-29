0f176f86404e2d3f7b7d43b3efd2a4b7287e7608
Julien Perichon <julienperichon023@gmail.com>
2022-07-29 17:13:35 +0200

    Add mixed precision support (#46)
    * Add .python-version to .gitignore for pyenv environments
    
    * Cast ClippedBinaryCrossEntropy parameters to same type as loss for mixed precision support
    
    * Set activation layer of LearntNorms to be of float32 dtype policy if global policy is mixed precision
    
    * Add ClippedBinaryCrossEntropy test for all float dtypes
    
    * Update CI config to install correct version of Tensorflow
    
    * Remove activation in last Dense layer

--- keras_fsl/losses/tests/gram_matrix_losses_test.py
+++ keras_fsl/losses/tests/gram_matrix_losses_test.py
@@ -140,6 +140,15 @@ class TestGramMatrixLoss:
             )
             np.testing.assert_almost_equal(tf_loss, np_loss, decimal=5)
 
+        @staticmethod
+        @pytest.mark.parametrize("dtype_policy", (tf.float16, tf.bfloat16, tf.float32, tf.float64))
+        def test_clipped_loss_computes_in_all_float_dtypes(dtype_policy, y_true, y_pred):
+            y_true_tensor = tf.convert_to_tensor(y_true)
+            y_pred_tensor = tf.convert_to_tensor(y_pred)
+            ClippedBinaryCrossentropy(lower=0.05, upper=0.95)(
+                tf.cast(y_true_tensor, dtype=dtype_policy), tf.cast(y_pred_tensor, dtype=dtype_policy)
+            )
+
         def test_max_loss_should_equal_literal_calculation(self, y_true, adjacency_matrix, y_pred):
             np_loss = np.max(-(adjacency_matrix * np.log(y_pred) + (1 - adjacency_matrix) * np.log(1 - y_pred)))
             tf_loss = MaxBinaryCrossentropy()(
